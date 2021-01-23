b98efa6b4495912caec3deaa00b139e4b5f9cbe3
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-01-23 15:29:57 -0600

    user profile seeds

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const seedUsers = require('./user-seeds');
 const seedMeals = require('./meal-seeds');
 const seedDiets = require('./diet-seeds');
 const seedMealDiets = require('./meal-diet-seeds');
+const seedUserProfile = require('./user-profile-seeds');
 
 const sequelize = require('../config/connection');
 
---
+++ seeds/user-profile-seeds.js
@@ -0,0 +1,24 @@
+
+const { userProfile } = require('../models');
+const userProfileData = [
+    {
+        user_id: 1,
+        firstName: 'Jennifer',
+        lastName: 'Ortiz',
+        streetAddress: '6305 Emerald Forest Dr',
+        city: 'Austin',
+        state: 'TX',
+        zipCode: '78745'
+   },
+   {
+        user_id: 2,
+        firstName: 'Herman',
+        lastName: 'Munster',
+        streetAddress: '1313 Mockingbird Lane',
+        city: 'San Marcos',
+        state: 'TX',
+        zipCode: '78666'
+   },
+];
+const seedUserProfile = () => userProfile.bulkCreate(userProfileData);
+module.exports = seedUserProfile;
