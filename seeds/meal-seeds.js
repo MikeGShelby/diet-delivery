f56ad512434e17f8c0b74763bad9316729d8e8ba
Mike Shelby <mikegshelby@gmail.com>
2021-01-24 14:52:23 -0600

    added additional images to images folder

--- seeds/meal-seeds.js
+++ seeds/meal-seeds.js
@@ -14,33 +14,33 @@ const mealData = [
   {
     title: 'Meal Item 3',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam posuere posuere.',
-    image: 'images/meal-image.jpg'
+    image: 'images/meal-image-3.jpg'
   },
   {
     title: 'Meal Item 4',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam posuere posuere.',
-    image: 'images/meal-image.jpg'
+    image: 'images/meal-image-4.jpg'
   },
   {
     title: 'Meal Item 5',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam posuere posuere.',
-    image: 'images/meal-image.jpg'
+    image: 'images/meal-image-5.jpg'
   },
   {
     title: 'Meal Item 6',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam posuere posuere.',
-    image: 'images/meal-image.jpg'
+    image: 'images/meal-image-6.jpg'
   },
 
   {
     title: 'Meal Item 7',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam posuere posuere.',
-    image: 'images/meal-image.jpg'
+    image: 'images/meal-image-3.jpg'
   },
   {
     title: 'Meal Item 8',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam posuere posuere.',
-    image: 'images/meal-image-2.jpg'
+    image: 'images/meal-image-8.jpg'
   },
   {
     title: 'Meal Item 9',
@@ -50,17 +50,17 @@ const mealData = [
   {
     title: 'Meal Item 10',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam posuere posuere.',
-    image: 'images/meal-image.jpg'
+    image: 'images/meal-image-2.jpg'
   },
   {
     title: 'Meal Item 11',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam posuere posuere.',
-    image: 'images/meal-image.jpg'
+    image: 'images/meal-image-3.jpg'
   },
   {
     title: 'Meal Item 12',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam posuere posuere.',
-    image: 'images/meal-image.jpg'
+    image: 'images/meal-image-4.jpg'
   }
 ];
 
