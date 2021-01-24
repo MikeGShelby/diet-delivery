0cc54ecc90b5fc24da1a9c9dab918fec35bb991f
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-01-24 14:00:59 -0600

    buttons to return menu items

--- public/javascript/filter-meals.js
+++ public/javascript/filter-meals.js
@@ -1,18 +1,43 @@
 // Add event listeners and callback functions here for diet filter buttons
 
-<<<<<<< HEAD
-const filterMeals = () => {
-    
-}
-    document.querySelector('filter-button').addEventListener('click', filterMeals);
-=======
 const buttons=document.querySelectorAll(".filter-button");
 
     for(let i=0; i<buttons.length; i++){
         buttons[i].addEventListener("click", e => {
-            console.log("buttonclicked", e.target.innerText);
+            let buttonId = e.target.id
+            console.log(buttonId);
+            buttonFilter(buttonId)
+            // console.log("buttonclicked", e.target.innerText);
         }
         )
     }
->>>>>>> f4281a13db017283605fae88a1c3ca17245bb496
+let renal =[...document.querySelectorAll('.renal')];
+    renal.unshift('renal')
+let diabetic =[...document.querySelectorAll('.diabetic')];
+    diabetic.unshift('diabetic')
+let FODMAP =[...document.querySelectorAll('.FODMAP')];
+    FODMAP.unshift('FODMAP')
+let foodarray = [renal, diabetic, FODMAP]
+
+function buttonFilter(foodPlan) {
+ 
+    console.log(foodarray)
+    let filterTerm;
+    foodarray.map(i => {
+        if (foodPlan === i[0]) {
+            filterTerm = i;
+        }
+    })
+    // let foodContainers = [...document.querySelectorAll(`.${foodPlan}`)];
+    // console.log(foodContainers);
+    const filteredContainer = document.querySelector('.filtered_containers')
+    filteredContainer.innerHTML = '';
+    const currentContainers = document.querySelector('.meal-list')
+
+    currentContainers.classList.add('hide')
+    console.log(filterTerm);
+    filterTerm.map(meal => {
+        filteredContainer.append(meal)
+    })
+}  
 
