c81d8b4220a343cff34ae4ba3b38b994f1ccf6e4
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-01-24 01:01:28 -0600

    started frontend javascript

--- public/javascript/filter-meals.js
+++ public/javascript/filter-meals.js
@@ -1,1 +1,7 @@
 // Add event listeners and callback functions here for diet filter buttons
+
+const filterMeals = () => {
+    
+}
+    document.querySelector('filter-button').addEventListener('click', filterMeals);
+
