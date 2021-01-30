1fd27cb0e943d5a084e04fb2b2682204d41c11cd
Mike Shelby <mikegshelby@gmail.com>
2021-01-29 22:29:58 -0600

    fixed issue with diet filter buttons that was causing selected diet word to appear with filtered meal cards. Also added focused styles to diet filter buttons

--- public/javascript/filter-meals.js
+++ public/javascript/filter-meals.js
@@ -1,22 +1,26 @@
 // Add event listeners and callback functions here for diet filter buttons
 
-const buttons=document.querySelectorAll(".filter-button");
+const buttons = document.querySelectorAll(".filter-button");
 
     for(let i=0; i<buttons.length; i++){
         buttons[i].addEventListener("click", e => {
+
             let buttonId = e.target.id
-            console.log(buttonId);
-            buttonFilter(buttonId)
+            // console.log(buttonId);
+            buttonFilter(buttonId);
             // console.log("buttonclicked", e.target.innerText);
-        }
-        )
+        })
     }
+
 let renal =[...document.querySelectorAll('.renal')];
     renal.unshift('renal')
+
 let diabetic =[...document.querySelectorAll('.diabetic')];
     diabetic.unshift('diabetic')
+
 let FODMAP =[...document.querySelectorAll('.FODMAP')];
-    FODMAP.unshift('FODMAP')
+FODMAP.unshift('FODMAP')
+
 let foodarray = [renal, diabetic, FODMAP]
 
 function buttonFilter(foodPlan) {
@@ -28,16 +32,24 @@ function buttonFilter(foodPlan) {
             filterTerm = i;
         }
     })
+
+    // const filteredMealsTitle = document.querySelector('.filtered-meals-title');
+    // filteredMealsTitle.innerHTML = '';
+
     // let foodContainers = [...document.querySelectorAll(`.${foodPlan}`)];
     // console.log(foodContainers);
-    const filteredContainer = document.querySelector('.filtered-list')
+    const filteredContainer = document.querySelector('.filtered-list');
     filteredContainer.innerHTML = '';
-    const currentContainers = document.querySelector('.meal-list')
 
-    currentContainers.classList.add('hide')
-    console.log(filterTerm);
-    filterTerm.map(meal => {
-        filteredContainer.append(meal)
+    const currentContainers = document.querySelector('.meal-list');
+    currentContainers.classList.add('hide');
+
+    const [, ...dietMeals] = filterTerm;
+    console.log(dietMeals);
+
+    dietMeals.map(meal => {
+        // filteredMealsTitle.innerHTML = `<h3>${foodPlan}</h3>`;
+        filteredContainer.append(meal);
     })
 }
 
